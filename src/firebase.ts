e6758ba153442c797b26f65d6319558b6dab495e
togarashi <tooogarasi@gmail.com>
2025-05-08 03:55:17 +0900

    0.0.8

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,13 @@
-import { initializeApp, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS!);
+const credentials = process.env.GOOGLE_CREDENTIALS;
+
+if (!credentials) {
+  throw new Error('Missing GOOGLE_CREDENTIALS environment variable');
+}
+
+const serviceAccount = JSON.parse(credentials);
 
 initializeApp({
   credential: cert(serviceAccount),
